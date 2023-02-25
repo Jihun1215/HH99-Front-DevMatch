3b9701ebc3062d2936d33b99e1dbb5a9d56760bf
VIVID89 <wskth0930@gmail.com>
2023-02-25 10:56:25 +0900

    detail 변경

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Layout from '../Components/Layout';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
