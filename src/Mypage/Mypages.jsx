8cfe1ed915e9016bbfe1ba3aeade57e43896826b
VIVID89 <wskth0930@gmail.com>
2023-02-28 00:08:32 +0900

    마이페이지 뷰 작업 완료

--- src/Mypage/Mypages.jsx
+++ src/Mypage/Mypages.jsx
@@ -3,8 +3,15 @@ import styled from 'styled-components';
 import { StContaitner } from '../Detail/Details';
 import Input from '../Components/Input';
 import Btn from '../Components/Button';
+import { useState } from 'react';
 
 function Mypages() {
+    const [selectedPart, setSelectedPart] = useState('');
+
+    const selectPartHandler = (e) => {
+        setSelectedPart(e.target.value);
+    };
+
     return (
         <div>
             <StContaitner>
@@ -14,16 +21,40 @@ function Mypages() {
                 </StInfoLayout>
                 <StInfoLayout>
                     <StInfoBox>NickName</StInfoBox>{' '}
-                    <Input type="text" style={{ marginRight: '20px' }} me />
+                    <Input type="text" style={{ border: 'none', marginRight: '20px' }} me />
                     <Btn me>변경</Btn>
                 </StInfoLayout>
 
                 <StInfoLayout>
-                    <StInfoBox>Stack</StInfoBox> <input type="select" />
+                    <StInfoBox>Part</StInfoBox>
+
+                    <StSelectBox>
+                        <h4>Backend</h4>
+                        <input
+                            style={{ width: '20px', height: '20px' }}
+                            type="radio"
+                            name="select"
+                            onChange={selectPartHandler}
+                            value="Backend"
+                            required
+                        />
+                        <h4>Frontend</h4>
+                        <input
+                            style={{ width: '20px', height: '20px' }}
+                            type="radio"
+                            name="select"
+                            onChange={selectPartHandler}
+                            value="Frontend"
+                            required
+                        />
+                        <Btn me>변경</Btn>
+                    </StSelectBox>
                 </StInfoLayout>
-                <StInfoLayout>
+                <StInfoLayout style={{ minHeight: '150px' }}>
                     {' '}
-                    <StInfoBox>Introduce</StInfoBox>{' '}
+                    <StInfoBox>Introduce</StInfoBox>
+                    <StTextArea type="text" />
+                    <Btn me>변경</Btn>
                 </StInfoLayout>
             </StContaitner>
         </div>
@@ -32,6 +63,7 @@ function Mypages() {
 
 export default Mypages;
 
+// 마이페이지 레이아웃
 const StInfoLayout = styled.div`
     width: 36.875rem;
     height: 3.75rem;
@@ -40,15 +72,40 @@ const StInfoLayout = styled.div`
     margin: auto;
     border-radius: 5px;
     margin-bottom: 20px;
+    /* border: 1px solid black; */
 `;
 
-const StInfoBox = styled.div`
-    width: 110px;
-    height: 50px;
+// 정보 박스 레이아웃
+const StInfoBox = styled.form`
+    width: 6.875rem;
+    height: 3.125rem;
     border-radius: 5px;
     box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;
     align-items: center;
     justify-content: center;
     display: flex;
     margin-right: 20px;
+`;
+
+// 파트 선택
+const StSelectBox = styled.div`
+    width: 460px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+    margin-left: 10px;
+    /* border: 1px solid black; */
+`;
+
+// 소개글
+const StTextArea = styled.textarea`
+    width: 340px;
+    height: 150px;
+    border-radius: 10px;
+    padding: 15px;
+    margin-right: 20px;
+    border: none;
+    outline: none;
 `;
