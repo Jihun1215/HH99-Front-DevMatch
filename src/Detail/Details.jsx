93c8f18ad29b68fb28575eaca7abd0b3b03187de
VIVID89 <wskth0930@gmail.com>
2023-02-28 23:56:37 +0900

    feat: 댓글 추가 서버통신 확인

--- src/Detail/Details.jsx
+++ src/Detail/Details.jsx
@@ -4,25 +4,24 @@ import Btn from '../Components/Button';
 import Input from '../Components/Input';
 import Sidebar from '../Components/Sidebar';
 import useInput from '../Hooks/useInput';
-import axios from 'axios';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import Modal from './Modal';
 import Cookies from 'js-cookie';
 import { useParams } from 'react-router-dom';
+import { api } from '../axios/api';
 
 function Details() {
     const params = useParams();
     const getToken = Cookies.get('token');
+    const sessionUserInfo = sessionStorage.getItem('userInfo');
+    const userInfo = JSON.parse(sessionUserInfo);
+    const currentUserName = userInfo.username;
+    const formData = new FormData();
 
     //api 부분
 
     //현재 접속자 정보
-    const [currentUser, setCurrentUser] = useState(null);
-
     useEffect(() => {
-        axios.get(`api/user/${params.id}`).then((response) => {
-            setCurrentUser(response.data);
-        });
         getDetailProject();
     }, [params]);
 
@@ -30,52 +29,69 @@ function Details() {
 
     const getDetailProject = async () => {
         try {
-            const response = await axios.get(`api/${params.id}`, {
+            const response = await api.get(`api/project/${params.id}`, {
                 headers: {
                     Authorization: getToken,
                 },
             });
-            console.log('response', response);
-            return response;
+            return response.data;
         } catch (error) {
-            console.log('GetProjectError: ', error);
+            console.log('GetDetailProjectError: ', error);
         }
     };
 
     //댓글 조회
-    const getDetailComment = async () => {
-        const response = await axios.get(`api/comment`);
-        return response.data;
-    };
+    // const getDetailComment = async () => {
+    //     try {
+    //         const response = await api.get(`api/comment/${params.id}`, {
+    //             headers: {
+    //                 Authorization: getToken,
+    //             },
+    //         });
+    //         return response.data;
+    //     } catch (error) {
+    //         console.log('getCommentError:', error);
+    //     }
+    // };
 
     //댓글 추가
     const addComment = async (newCommnet) => {
-        await axios.post(`api/comment`, newCommnet);
+        try {
+            const response = await api.post(`api/comment/${params.id}`, newCommnet, {
+                headers: {
+                    Authorization: getToken,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.log('addCommentError:', error);
+        }
     };
 
     // 댓글 수정
     const editComment = async (id) => {
-        await axios.put(`api/comment/${id}`, {
+        await api.put(`api/comment/${id}`, {
             comment: editCmt,
         });
     };
 
     // 댓글 삭제
     const deleteComment = async (id) => {
-        await axios.delete(`api/comment/${id}`);
+        await api.delete(`api/comment/${id}`);
     };
 
     const addLikeCount = async (newlike) => {
-        await axios.post(`api/project/like/${params.id}`, newlike);
+        await api.post(`api/project/like/${params.id}`, newlike);
     };
 
     //리액트 쿼리 부분
 
     //프로젝트 데이터
     const projectData = useQuery('project', getDetailProject);
-
+    console.log('projectdata', projectData);
     //댓글 데이터
-    const commentData = useQuery('comment', getDetailComment);
+    const commentData = useQuery('comment', addComment);
+    console.log('commentData', commentData);
 
     const queryclient = useQueryClient();
     const mutation = useMutation(addComment, {
@@ -110,6 +126,9 @@ function Details() {
     const [likeCount, setLikeCount] = useState(0);
     const [showEdit, setShowEdit] = useState(false);
 
+    //댓글 formdata로 집어넣기
+    formData.append('content', comment);
+
     // 임시 좋아요 카운트 버튼 총 카운트 보내야함
     // 좋아요는 유저 한명당 1회만 가능한지?
     const likeCountButtonHandler = () => {
@@ -117,15 +136,12 @@ function Details() {
     };
 
     //댓글 추가 버튼
-    const onAddCommentBtnhandler = (e) => {
+    const onAddCommentBtnhandler = () => {
         if (comment === '') {
             alert('빈칸입니다.');
             return false;
         } else {
-            mutation.mutate({
-                comment: comment,
-                username: currentUser.username,
-            });
+            mutation.mutate(formData);
             setEditCmt(comment);
             setComment('');
         }
@@ -136,7 +152,7 @@ function Details() {
     const onShowInputHandler = (id) => {
         commentData.data.find((item) => {
             if (item.id === id) {
-                if (item.username === currentUser.username) {
+                if (item.username === currentUserName) {
                     setEditCmt(item.comment);
                     setShowInput((prevState) => (prevState === id ? true : id));
                     setShowCancel((prevState) => (prevState === id ? false : id));
@@ -170,18 +186,14 @@ function Details() {
 
     //댓글 삭제 버튼
     const deleteButtonHandler = (id) => {
-        commentData.data.find((item) => {
-            if (item.id === id) {
-                if (item.username === currentUser.username) {
-                    mutation3.mutate(id);
-                    setShowInput(!showInput);
-                    setShowCancel(!showCancel);
-                    setShowEdit(!showEdit);
-                } else {
-                    alert('작성자만 삭제 가능합니다');
-                }
-            }
-        });
+        if (commentData.username === currentUserName) {
+            mutation3.mutate(id);
+            setShowInput(!showInput);
+            setShowCancel(!showCancel);
+            setShowEdit(!showEdit);
+        } else {
+            alert('작성자만 삭제 가능합니다');
+        }
     };
 
     return (
@@ -189,124 +201,127 @@ function Details() {
             <Sidebar>
                 <Modal />
             </Sidebar>
-
-            <StContaitner>
-                <StImageBox>{projectData.data?.image}</StImageBox>
-                <div>
-                    <StRecruitList>
-                        <h3>{projectData.data?.title}</h3>
-                    </StRecruitList>
-                    <StRecruitList>
-                        <StMiniLayout>
-                            <h3>Backend</h3>
-                        </StMiniLayout>
-                        <StMiniLayout>
-                            <h3>{projectData.data?.backEndStack}</h3>
-                        </StMiniLayout>
-                        <StMiniLayout>
-                            <h3>{projectData.data?.backendMember}</h3>
-                        </StMiniLayout>
-                    </StRecruitList>
-                    <StRecruitList>
-                        <StMiniLayout>
-                            <h3>Frontend</h3>
-                        </StMiniLayout>
-                        <StMiniLayout>
-                            <h3>{projectData.data?.frontEndStack}</h3>
-                        </StMiniLayout>
-                        <StMiniLayout>
-                            <h3>{projectData.data?.frontendMember}</h3>
-                        </StMiniLayout>
-                    </StRecruitList>
-                </div>
-                <StDetailLayout>
-                    <h3>Detail</h3>
-                </StDetailLayout>
-                <StLikeNumber>{projectData.data?.likeCount}</StLikeNumber>
-                <Btn me onClick={likeCountButtonHandler}>
-                    LIKE❤️
-                </Btn>
-                <StDetailBox>{projectData.data?.content}</StDetailBox>
-
-                <StCommentLayout>
-                    <StMiniLayout>
-                        <h3>COMMENT</h3>
-                    </StMiniLayout>
-                    <Input
-                        me
-                        type="text"
-                        placeholder="코멘트를 남겨주세요"
-                        style={{ border: 'none' }}
-                        value={comment}
-                        onChange={onCommentHandler}
-                    />
-                    <Btn me onClick={onAddCommentBtnhandler}>
-                        {' '}
-                        add
+            {projectData.isLoading === false && (
+                <StContaitner>
+                    <StImageBox>
+                        <img src={projectData.data.result?.imageUrl} />
+                    </StImageBox>
+                    <div>
+                        <StRecruitList>
+                            <h3>{projectData.data.result?.title}</h3>
+                        </StRecruitList>
+                        <StRecruitList>
+                            <StMiniLayout>
+                                <h3>Backend</h3>
+                            </StMiniLayout>
+                            <StMiniLayout>
+                                <h3>{projectData.data.result?.backEndStack}</h3>
+                            </StMiniLayout>
+                            <StMiniLayout>
+                                <h3>{projectData.data.result?.backendMember}</h3>
+                            </StMiniLayout>
+                        </StRecruitList>
+                        <StRecruitList>
+                            <StMiniLayout>
+                                <h3>Frontend</h3>
+                            </StMiniLayout>
+                            <StMiniLayout>
+                                <h3>{projectData.data.result?.frontEndStack}</h3>
+                            </StMiniLayout>
+                            <StMiniLayout>
+                                <h3>{projectData.data.result?.frontendMember}</h3>
+                            </StMiniLayout>
+                        </StRecruitList>
+                    </div>
+                    <StDetailLayout>
+                        <h3>Detail</h3>
+                    </StDetailLayout>
+                    <StLikeNumber>{projectData.data.result?.likeCount}</StLikeNumber>
+                    <Btn me onClick={likeCountButtonHandler}>
+                        LIKE❤️
                     </Btn>
-                </StCommentLayout>
-                {commentData.data?.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            <StCommentBox>
-                                <StMiniLayout>
-                                    <h3>{item.username}</h3>
-                                </StMiniLayout>
-                                <StCommentListBox>
-                                    {showInput === item.id ? (
-                                        <Input
-                                            me
-                                            type="text"
-                                            value={editCmt}
-                                            onChange={onEditCmt}
-                                            placeholder="수정 내용을 입력해주세요"
-                                            style={{ color: '#F38BA0', border: 'none' }}
-                                        />
-                                    ) : (
-                                        <h4>{item.comment} </h4>
-                                    )}
-                                </StCommentListBox>
-                                <StMiniLayout style={{ gap: '5px' }}>
-                                    {showEdit === item.id ? (
-                                        <Btn sm onClick={() => editButtonHandler(item.id)}>
-                                            수정하기{' '}
-                                        </Btn>
-                                    ) : (
-                                        <Btn
-                                            sm
-                                            onClick={() => {
-                                                onShowInputHandler(item.id);
-                                            }}
-                                        >
-                                            수정
-                                        </Btn>
-                                    )}
+                    <StDetailBox>{projectData.data.result?.content}</StDetailBox>
 
-                                    {showCancel === item.id ? (
-                                        <Btn
-                                            sm
-                                            onClick={cancelButtonHandler}
-                                            style={{ backgroundColor: '#F38BA0' }}
-                                        >
-                                            취소
-                                        </Btn>
-                                    ) : (
-                                        <Btn
-                                            sm
-                                            style={{ backgroundColor: '#F38BA0' }}
-                                            onClick={() => {
-                                                deleteButtonHandler(item.id);
-                                            }}
-                                        >
-                                            삭제
-                                        </Btn>
-                                    )}
-                                </StMiniLayout>
-                            </StCommentBox>
-                        </div>
-                    );
-                })}
-            </StContaitner>
+                    <StCommentLayout>
+                        <StMiniLayout>
+                            <h3>COMMENT</h3>
+                        </StMiniLayout>
+                        <Input
+                            me
+                            type="text"
+                            placeholder="코멘트를 남겨주세요"
+                            style={{ border: 'none' }}
+                            value={comment}
+                            onChange={onCommentHandler}
+                        />
+                        <Btn me onClick={onAddCommentBtnhandler}>
+                            {' '}
+                            add
+                        </Btn>
+                    </StCommentLayout>
+                    {commentData.data?.map((item) => {
+                        return (
+                            <div key={item.id}>
+                                <StCommentBox>
+                                    <StMiniLayout>
+                                        <h3>{item.username}</h3>
+                                    </StMiniLayout>
+                                    <StCommentListBox>
+                                        {showInput === item.id ? (
+                                            <Input
+                                                me
+                                                type="text"
+                                                value={editCmt}
+                                                onChange={onEditCmt}
+                                                placeholder="수정 내용을 입력해주세요"
+                                                style={{ color: '#F38BA0', border: 'none' }}
+                                            />
+                                        ) : (
+                                            <h4>{item.comment} </h4>
+                                        )}
+                                    </StCommentListBox>
+                                    <StMiniLayout style={{ gap: '5px' }}>
+                                        {showEdit === item.id ? (
+                                            <Btn sm onClick={() => editButtonHandler(item.id)}>
+                                                수정하기{' '}
+                                            </Btn>
+                                        ) : (
+                                            <Btn
+                                                sm
+                                                onClick={() => {
+                                                    onShowInputHandler(item.id);
+                                                }}
+                                            >
+                                                수정
+                                            </Btn>
+                                        )}
+
+                                        {showCancel === item.id ? (
+                                            <Btn
+                                                sm
+                                                onClick={cancelButtonHandler}
+                                                style={{ backgroundColor: '#F38BA0' }}
+                                            >
+                                                취소
+                                            </Btn>
+                                        ) : (
+                                            <Btn
+                                                sm
+                                                style={{ backgroundColor: '#F38BA0' }}
+                                                onClick={() => {
+                                                    deleteButtonHandler(item.id);
+                                                }}
+                                            >
+                                                삭제
+                                            </Btn>
+                                        )}
+                                    </StMiniLayout>
+                                </StCommentBox>
+                            </div>
+                        );
+                    })}
+                </StContaitner>
+            )}
         </>
     );
 }
@@ -333,6 +348,10 @@ const StImageBox = styled.div`
     box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px;
     border-radius: 0.625rem;
     margin-right: 20px;
+    > img {
+        width: 250px;
+        height: 200px;
+    }
 `;
 
 // 상세페이지 모집요강 리스트
@@ -388,15 +407,6 @@ const StMiniLayout = styled.div`
     justify-content: center;
     align-items: center;
     /* border: 1px solid black; */
-`;
-
-//stack Layout
-const StStackLayout = styled.div`
-    width: 20rem;
-    height: 3.75rem;
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
 `;
 
 //comment layout
